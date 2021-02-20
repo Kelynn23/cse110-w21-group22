d8f901af2d9e91c8fbf258087b16c7e4c78f9a59
KevinJiechengWang <47407719+KevinJiechengWang@users.noreply.github.com>
2021-02-20 13:46:47 -0800

    unbreaking whatever i broke in dev earlier

--- timer.js
+++ timer.js
@@ -2,6 +2,9 @@ const startButton = document.getElementById("starttimer");
 startButton.addEventListener('click', startTimer);
 
 const displayTimer = document.getElementById('timerDisplay');
+const displayTitle = document.getElementById('pageTitle');
+const autoStartSetting = document.getElementById('autostart');
+
 /**const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 incrementButton.addEventListener('click', increment);
@@ -12,6 +15,7 @@ var numPomos = 0;
 var timerInterval;
 var startTime = focusTime;
 var mode = 0;
+var modeStr = '';
 var timerStatus = 0; //0 is not started, -1 is stopped, 1 is running
 var focusTime = 10;
 var shortBreak = 5;
@@ -24,34 +28,51 @@ function startTimer() {
     if(timerStatus == 0) {
         pomoMode();
         timerInterval = setInterval(timer, 1000);
-        startButton.innerHTML = "Stop";
+        startButton.innerHTML = "Reset";
         //timer is now running, set flag
         timerStatus = 1;
     }
-    //timer was stopped
-    else if (timerStatus == 1){
-        stopTimer();
-    }
     //timer is about to be reset
-    else if (timerStatus == -1) {
+    else if (timerStatus == 1) {
         resetTimer();
+        pomoMode();
     }
-}
-
-//stops the timer and allows user to reset their pomo session
-function stopTimer() {
-    clearInterval(timerInterval);
-    timerStatus = -1;
-    startButton.innerHTML = "Reset";
 }
 
 //resets the timer to original time and mode to work time
 function resetTimer() {
+    clearInterval(timerInterval);
     startTime = focusTime;
+    modeStr = 'Working';
     displayTime();
-    mode = 0;
     timerStatus = 0;
     startButton.innerHTML = "Start";
+}
+
+//sets the timer to a different value based on what triggered the change
+//only allows changes when timer is not running
+function setTimer(operation) {
+    //timer is running, do not allow changes
+    if(timerStatus != 0) {
+      console.log("timer is running, disabled setting time");
+      return;
+    }
+  
+    if(operation == "increment") {
+        focusTime += 60;
+    }
+    else if (operation == "decrement") {
+        if(focusTime > 60) {
+            focusTime -=60;
+        }
+        else {
+            focusTime = 0;
+        }
+    }
+  
+    startTime = focusTime;
+    //redraw the time
+    displayTime();
 }
 
 //main driver for timer functionality
@@ -67,40 +88,89 @@ function timer()
         clearInterval(timerInterval);
         startButton.innerHTML = "Start";
         timerStatus = 0;
-        if(numPomos != 0 && numPomos % numPomosToLongBreak == 0)
-        {
-            //long break
-            mode = 10000;
-            numPomos += 1;
-            document.getElementById('complete').innerHTML = numPomos + " Pomos Finished";
+
+        //notification to users at session end
+        if(mode == 0){
+            displayTitle.innerHTML = 'Work Session Ended';
+            /* only create an alert if AutoStart is turned off, 
+               (the next round won't start until the user manually accepts the alert) */
+            if(!autoStartSetting.checked) {
+                alert('Work Session Ended');
+            }
         }
-        else if(mode == 1) {
-            //work mode
-            mode = 0;
-            numPomos += 1;
-            document.getElementById('complete').innerHTML = numPomos + " Pomos Finished";
+        else if(mode == 1){
+            displayTitle.innerHTML = 'Short Break Ended';
+            /* only create an alert if AutoStart is turned off, 
+               (the next round won't start until the user manually accepts the alert) */
+            if(!autoStartSetting.checked) {
+                alert('Short Break Ended');
+            }
         }
-        else {
-            //short break
-            mode = 1;
+        else{
+            displayTitle.innerHTML = 'Long Break Ended';
+            /* only create an alert if AutoStart is turned off, 
+               (the next round won't start until the user manually accepts the alert) */
+            if(!autoStartSetting.checked) {
+                alert('Long Break Ended');
+            }
         }
 
+        //working -> long break
+        if(numPomos % numPomosToLongBreak == numPomosToLongBreak - 1 
+            && mode == 0) {
+            mode = 2;
+            //display the time for next mode
+            pomoMode(); 
+        }
+        //short break -> working
+        else if(mode == 1) {
+            //increment numPomos after short break
+            numPomos += 1;
+            document.getElementById('complete').innerHTML = numPomos + " Pomos Finished";
+            mode = 0;
+            //display the time for next mode
+            pomoMode();
+        }
+        else {
+            //working -> short break
+            if(mode == 0){
+                mode = 1;
+                pomoMode();
+            }
+            //long break -> working
+            else{
+                //increment numPomos after long break
+                numPomos += 1;
+                document.getElementById('complete').innerHTML = numPomos + " Pomos Finished";
+                mode = 0;
+                pomoMode();
+            }
+        }
+
+        // Start the next round if the AutoStart Setting is turned on
+        if(autoStartSetting.checked) {
+            startTimer();
+        }
     }
 }
 
+//To set mode for the next round of timer
 //0: work, 1: short break, 2:long break
 function pomoMode()
 {
     if(mode == 0) {
-        startTime = 10;
+        startTime = focusTime;
+        modeStr = 'Working';
         displayTime();
     }
     else if(mode == 1) {
         startTime = shortBreak;
+        modeStr = 'Short Break';
         displayTime();
-
-    } else {
+    } 
+    else {
         startTime = longBreak;
+        modeStr = 'Long Break';
         displayTime();
     }
 }
@@ -108,11 +178,12 @@ function pomoMode()
 function displayTime() {
     let seconds = startTime % 60;
     let minutes = Math.floor((startTime / 60));
-    if(seconds < 10) seconds = '0' + seconds;
 
+    if(seconds < 10) seconds = '0' + seconds;
     if(minutes < 10) minutes = '0' + minutes;
 
     displayTimer.innerHTML = minutes + ":" + seconds;
+    displayTitle.innerHTML = '(' + minutes + ':' + seconds + ')' + modeStr;
 }
 
 /** 
