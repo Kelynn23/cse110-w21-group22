62bb7199e88a6c94bfe32dd5c0915405078f29ef
TangYihe <yihe1125@gmail.com>
2021-03-05 00:57:17 -0800

    added some cypress tests

--- cypress/integration/timer-tests.js
+++ cypress/integration/timer-tests.js
@@ -19,3 +19,25 @@ beforeEach(() => {
         expect($el).to.have.attr('text','00:20');
       })
   });
+
+  it('Timer Run Update Display',() =>{
+    cy.get('#starttimer').click();
+    cy.wait(5000);
+    cy.get('#timerDisplay').then( () => function($el) {
+        expect($el).to.have.attr('text','00:05');
+      })
+  });
+
+  it('Timer End Send Alert',() =>{
+    cy.get('#starttimer').click();
+    cy.wait(10000);
+    cy.on('window:alert', (str) => {
+        expect(str).to.equal('Work Session Ended');
+      })
+  });
+
+  it('Timer Run Enable Reset Button',() =>{
+    cy.get('#starttimer').click();
+    cy.wait(1000);
+    cy.get('#resettimer').should('be.enabled');
+  });
