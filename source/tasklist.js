1295a17be6c5383e6ac3c7ae7b35438fa45dc301
KevinJiechengWang <47407719+KevinJiechengWang@users.noreply.github.com>
2021-03-06 18:38:06 -0800

    Add files via upload

---
+++ source/tasklist.js
@@ -0,0 +1,35 @@
+const MAX_TASKLIST_SIZE = 20;
+const input = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addtask');
+addTaskButton.addEventListener('click', addTask);
+
+const taskList = document.getElementById('tlist');
+
+function addTask() {
+  //press to show input field
+  if(window.getComputedStyle(input).getPropertyValue('display') == "none") {
+    //maximum size the tasklist can be is 20
+    if(taskList.getElementsByTagName("LI").length > MAX_TASKLIST_SIZE) {
+      alert("You've got too many tasks.");
+      return;
+    }
+    input.style.display = "block";
+  }
+  //press after input given
+  else {  
+    //get user input and append a list item containing that input
+    let item = document.createElement('li');
+    itemText = document.getElementById('task').value;
+    //check to ensure field is not empty
+    if(itemText === '' || itemText === null) {
+      return;
+    }
+    item.innerHTML = itemText;
+    item.addEventListener('click', function() { //onclick the entry will strikethrough
+      item.classList.add('taskclicked');
+    })
+
+    taskList.appendChild(item); //add to list
+    input.style.display = "none"; //hide input field
+  }
+}
