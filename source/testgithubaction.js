aa87733a8cb84df785f68839e01ad16ee46a5c88
Chanly Ly <lychanly4@gmail.com>
2021-03-14 22:21:11 -0700

    testing github action

--- source/testgithubaction.js
+++ source/testgithubaction.js
@@ -1,10 +1,12 @@
 /**
-* @author group 22
-/
+ * @file testing github action 
+ * @author group 22
+ */
 
-/*
-*This does nothing
-*/
-function dummy()  {
 
+/**
+ * This is a dummy function to help test github action
+ */
+function dummyFunc() {
+    let dummyVar = 1;
 }
